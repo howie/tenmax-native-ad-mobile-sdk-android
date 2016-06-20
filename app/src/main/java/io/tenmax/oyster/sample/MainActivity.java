257ea454ae935054ee3a5bbded67c93b0219b3ba
liq6 <liq6@liquable.com>
2016-06-20 17:59:57 +0800

    modify layout and use oyster json data

--- app/src/main/java/io/tenmax/oyster/sample/MainActivity.java
+++ app/src/main/java/io/tenmax/oyster/sample/MainActivity.java
@@ -1,7 +1,6 @@
 package io.tenmax.oyster.sample;
 
-import java.util.List;
-
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,7 +8,6 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import io.tenmax.oyster.AdListener;
@@ -38,34 +36,28 @@ public class MainActivity extends AppCompatActivity {
     refresh.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        refreshAd(requestAppInstallAds.isChecked(),
-            requestContentAds.isChecked());
+        refreshAd(requestAppInstallAds.isChecked(), requestContentAds.isChecked());
       }
     });
 
-    refreshAd(requestAppInstallAds.isChecked(),
-        requestContentAds.isChecked());
+    refreshAd(requestAppInstallAds.isChecked(), requestContentAds.isChecked());
   }
 
-  private void populateContentAdView(NativeContentAd nativeContentAd,
-      NativeContentAdView adView) {
+  private void populateContentAdView(NativeContentAd nativeContentAd, NativeContentAdView adView) {
     adView.setHeadlineView(adView.findViewById(R.id.contentad_headline));
     adView.setImageView(adView.findViewById(R.id.contentad_image));
     adView.setBodyView(adView.findViewById(R.id.contentad_body));
-    adView.setCallToActionView(adView.findViewById(R.id.contentad_call_to_action));
     adView.setLogoView(adView.findViewById(R.id.contentad_logo));
     adView.setAdvertiserView(adView.findViewById(R.id.contentad_advertiser));
 
     // Some assets are guaranteed to be in every NativeContentAd.
     ((TextView) adView.getHeadlineView()).setText(nativeContentAd.getHeadline());
     ((TextView) adView.getBodyView()).setText(nativeContentAd.getBody());
-    ((TextView) adView.getCallToActionView()).setText(nativeContentAd.getCallToAction());
     ((TextView) adView.getAdvertiserView()).setText(nativeContentAd.getAdvertiser());
 
-    List<NativeAd.Image> images = nativeContentAd.getImages();
-
-    if (images != null && images.size() > 0) {
-      ((ImageView) adView.getImageView()).setImageDrawable(images.get(0).getDrawable());
+    if (nativeContentAd.getImage().getDrawable() != null) {
+      ((ImageView) adView.getImageView()).setImageDrawable(nativeContentAd.getImage()
+          .getDrawable());
     }
 
     // Some aren't guaranteed, however, and should be checked.
@@ -87,55 +79,25 @@ public class MainActivity extends AppCompatActivity {
     adView.setHeadlineView(adView.findViewById(R.id.appinstall_headline));
     adView.setImageView(adView.findViewById(R.id.appinstall_image));
     adView.setBodyView(adView.findViewById(R.id.appinstall_body));
-    adView.setCallToActionView(adView.findViewById(R.id.appinstall_call_to_action));
     adView.setLogoView(adView.findViewById(R.id.appinstall_app_icon));
-    adView.setPriceView(adView.findViewById(R.id.appinstall_price));
-    adView.setStarRatingView(adView.findViewById(R.id.appinstall_stars));
-    adView.setStoreView(adView.findViewById(R.id.appinstall_store));
 
     // Some assets are guaranteed to be in every NativeAppInstallAd.
     ((TextView) adView.getHeadlineView()).setText(nativeAppInstallAd.getHeadline());
     ((TextView) adView.getBodyView()).setText(nativeAppInstallAd.getBody());
-    ((Button) adView.getCallToActionView()).setText(nativeAppInstallAd.getCallToAction());
-    ((ImageView) adView.getLogoView()).setImageDrawable(nativeAppInstallAd.getLogo()
-        .getDrawable());
+    ((ImageView) adView.getLogoView()).setImageDrawable(nativeAppInstallAd.getLogo().getDrawable());
 
-    List<NativeAd.Image> images = nativeAppInstallAd.getImages();
-
-    if (images.size() > 0) {
-      ((ImageView) adView.getImageView()).setImageDrawable(images.get(0).getDrawable());
+    if (nativeAppInstallAd.getImage().getDrawable() != null) {
+      ((ImageView) adView.getImageView()).setImageDrawable(nativeAppInstallAd.getImage()
+          .getDrawable());
     }
-
-    // Some aren't guaranteed, however, and should be checked.
-    if (nativeAppInstallAd.getPrice() == null) {
-      adView.getPriceView().setVisibility(View.INVISIBLE);
-    } else {
-      adView.getPriceView().setVisibility(View.VISIBLE);
-      ((TextView) adView.getPriceView()).setText(nativeAppInstallAd.getPrice());
-    }
-
-    if (nativeAppInstallAd.getStore() == null) {
-      adView.getStoreView().setVisibility(View.INVISIBLE);
-    } else {
-      adView.getStoreView().setVisibility(View.VISIBLE);
-      ((TextView) adView.getStoreView()).setText(nativeAppInstallAd.getStore());
-    }
-
-    if (nativeAppInstallAd.getStarRating() == null) {
-      adView.getStarRatingView().setVisibility(View.INVISIBLE);
-    } else {
-      ((RatingBar) adView.getStarRatingView())
-          .setRating(nativeAppInstallAd.getStarRating().floatValue());
-      adView.getStarRatingView().setVisibility(View.VISIBLE);
-    }
-
     // Assign native ad object to the native view.
     adView.setNativeAd(nativeAppInstallAd);
   }
 
   private void refreshAd(boolean requestAppInstallAds, boolean requestContentAds) {
     if (!requestAppInstallAds && !requestContentAds) {
-      Toast.makeText(this, "At least one ad format must be checked to request an ad.",
+      Toast.makeText(this,
+          "At least one ad format must be checked to request an ad.",
           Toast.LENGTH_SHORT).show();
       return;
     }
@@ -146,10 +108,9 @@ public class MainActivity extends AppCompatActivity {
       builder.forAppInstallAd(new NativeAppInstallAd.OnAppInstallAdLoadedListener() {
         @Override
         public void onAppInstallAdLoaded(NativeAppInstallAd nativeAppInstallAd) {
-          FrameLayout frameLayout =
-              (FrameLayout) findViewById(R.id.adPlace);
-          NativeAppInstallAdView adView = (NativeAppInstallAdView) getLayoutInflater()
-              .inflate(R.layout.ad_install, null);
+          FrameLayout frameLayout = (FrameLayout) findViewById(R.id.adPlace);
+          NativeAppInstallAdView adView = (NativeAppInstallAdView) getLayoutInflater().inflate(R.layout.ad_install,
+              null);
           populateAppInstallAdView(nativeAppInstallAd, adView);
           frameLayout.removeAllViews();
           frameLayout.addView(adView);
@@ -161,10 +122,9 @@ public class MainActivity extends AppCompatActivity {
       builder.forContentAd(new NativeContentAd.OnContentAdLoadedListener() {
         @Override
         public void onContentAdLoaded(NativeContentAd nativeContentAd) {
-          FrameLayout frameLayout =
-              (FrameLayout) findViewById(R.id.adPlace);
-          NativeContentAdView adView = (NativeContentAdView) getLayoutInflater()
-              .inflate(R.layout.ad_content, null);
+          FrameLayout frameLayout = (FrameLayout) findViewById(R.id.adPlace);
+          NativeContentAdView adView = (NativeContentAdView) getLayoutInflater().inflate(R.layout.ad_content,
+              null);
           populateContentAdView(nativeContentAd, adView);
           frameLayout.removeAllViews();
           frameLayout.addView(adView);
@@ -174,9 +134,10 @@ public class MainActivity extends AppCompatActivity {
 
     AdLoader adLoader = builder.withAdListener(new AdListener() {
       @Override
-      public void onAdFailedToLoad(int errorCode) {
-        Toast.makeText(MainActivity.this, "Failed to load native ad: "
-            + errorCode, Toast.LENGTH_SHORT).show();
+      public void onAdFailedToLoad(Exception e) {
+        Toast.makeText(MainActivity.this,
+            "Failed to load native ad: " + e.getMessage(),
+            Toast.LENGTH_SHORT).show();
       }
     }).build();
 
